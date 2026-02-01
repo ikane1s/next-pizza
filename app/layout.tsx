33e534bcae0e13907ed4641a0f9d75707a96b6f7
ikane1s <erushew27@gmail.com>
2026-02-01 14:31:05 +0700

    Initial commit: Next.js pizza project setup

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+import { Nunito } from 'next/font/google';
+
+const nunito = Nunito({
+  subsets: ['cyrillic'],
+  variable: '--font-nunito',
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: 'Next Pizza',
+  description: 'Next Pizza',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${nunito.variable} ${nunito.variable} antialiased`}>
+        <main className="min-h-screen">{children}</main>
+      </body>
+    </html>
+  );
+}
